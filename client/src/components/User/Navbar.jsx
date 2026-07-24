89d0e28cbe9bf862aea757482d4e2963a0ca83e8
arshadvkb <arshadvkb118@gmail.com>
2026-07-24 18:08:53 +0530

    fix: correct logout button functionality and improve code formatting in Navbar and User_router components

--- client/src/components/User/Navbar.jsx
+++ client/src/components/User/Navbar.jsx
@@ -3,10 +3,12 @@ const Navbar = () => {
     const user=localStorage.getItem("Active User")
   return (
     <div>
-    <button onClick={()=>localStorage.clear()}>logout</button>
-{user}
+      <button onClick={() => localStorage.removeItem("Active User")}>
+        logout
+      </button>
+      {user}
     </div>
-  )
+  );
 }
 
 export default Navbar
