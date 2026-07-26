55ebacb39f466864e02a11812dfc83524902b792
arshadvkb <arshadvkb118@gmail.com>
2026-07-26 21:58:05 +0530

    refactor: clean up formatting and improve code readability in View_Products component

--- client/src/components/User/View_Products.jsx
+++ client/src/components/User/View_Products.jsx
@@ -1,26 +1,27 @@
 import { useContext, useEffect } from "react";
 import { ProductContext } from "../../context/Product_Context";
 
-
 const View_Products = () => {
-   const { state, fetchProduct } = useContext(ProductContext);
-   useEffect(() => {
-     fetchProduct();
-   }, []);
+  const { state, fetchProduct } = useContext(ProductContext);
+  useEffect(() => {
+    fetchProduct();
+  }, []);
 
-   console.log(state.products);
+  console.log(state.products);
 
-   return (
-     <div>
-       {state.products.map((product) => (
-         <div key={product.id} className="">
-           <img src={product.images[1]} alt="" className="h-10 w-10" />
-           <h2>{product.name}</h2>
-           <p>{product.price}</p>
-         </div>
-       ))}
-     </div>
-   );
-}
+  return (
+    <div className="pt-20">
+      <div className="grid grid-cols-4 pl-64 ">
+        {state.products.map((product) => (
+          <div key={product.id} className="">
+            <img src={product.images[1]} alt="" className="h-10 w-10" />
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default View_Products
+export default View_Products;
